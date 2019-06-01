b7b1c0dc20e0afd623bc8c295bdf8f9eeca5eb52
Vincent Humeau <hello@vincent-humeau.com>
2019-06-01 08:16:48 +0100

    Refactor sortObjectBy method

--- src/App.vue
+++ src/App.vue
@@ -24,15 +24,10 @@ export default {
   },
   methods: {
     sortObjectBy(property) {
-      let sortOrder = 1;
-      if (property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-      }
       return function(a, b) {
-        var result =
-          a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-        return result * sortOrder;
+        return a[property] == b[property]
+          ? 0
+          : +(a[property] > b[property]) || -1;
       };
     },
     getPopularMovies() {
