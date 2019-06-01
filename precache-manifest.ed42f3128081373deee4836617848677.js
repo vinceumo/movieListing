356f04c51d98ea632af7b8b21ac0112a7cf76771
Vincent Humeau <hello@vincent-humeau.com>
2019-06-01 16:08:50 +0100

    Updates

---
+++ precache-manifest.ed42f3128081373deee4836617848677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2c556aee7c3a2d807231",
+    "url": "/js/chunk-vendors.cab436ef.js"
+  },
+  {
+    "revision": "6623d4337640e7bd6616",
+    "url": "/js/app.96195148.js"
+  },
+  {
+    "revision": "181697c1dcdab4667ad9dce884e40971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6623d4337640e7bd6616",
+    "url": "/css/app.90a98107.css"
+  }
+];
