e0e7bae4344e908ba8a9d2b7cef1d9888c654b8f
Vincent Humeau <hello@vincent-humeau.com>
2019-06-01 17:55:04 +0100

    Added missing pwa settings

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,21 @@ module.exports = {
       addStyleResource(config.module.rule("scss").oneOf(type))
     );
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/movieListing/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/movieListing/" : "/",
+  pwa: {
+    name: "Films showing in cinemas",
+    themeColor: "hsl(237, 38%, 33%)",
+    msTileColor: "hsl(237, 38%, 33%)",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon.png",
+      maskIcon: "img/icons/safari-pinned-tab.svg",
+      msTileImage: "img/icons/mstile-150x150.png"
+    }
+  }
 };
 
 function addStyleResource(rule) {
